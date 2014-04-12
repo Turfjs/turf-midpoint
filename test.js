f8ac9deb4d3360e4bc5524a6e5c64973e271f72a
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-12 12:23:11 -0400

    test refactor

--- test.js
+++ test.js
@@ -6,76 +6,22 @@ test('midpoint', function(t){
   var pt1 = t.point(0,0)
   var pt2 = t.point(10, 0)
   var expectedMidPoint = t.point(5, 0)
-  midpoint(pt1, pt2, function(err, midpoint){
-  _.isEqual(midpoint, expectedMidPoint).should.be.true
+  var expectedMidPoint = midpoint(pt1, pt2, function(err, midpoint){
+  t.deepEqual(midpoint, expectedMidPoint, 'should return the halfway point of a horizontal line starting off 0,0')
+
+  var line = t.linestring([[0,0], [0,10]])
+  var pt1 = t.point(0,0)
+  var pt2 = t.point(0,10)
+  var expectedMidPoint = t.point(0, 5)
+  var expectedMidPoint = midpoint(pt1, pt2, function(err, midpoint){
+  t.deepEqual(midpoint, expectedMidPoint, 'should return the halfway point of a vertical line starting off 0,0')
+
+  var line = t.linestring([[1,1], [11,11]])
+  var pt1 = t.point(1,1)
+  var pt2 = t.point(11,11)
+  var expectedMidPoint = t.point(6, 6)
+  t.midpoint(pt1, pt2, function(err, midpoint){
+  t.deepEqual(midpoint, expectedMidPoint, 'should return the halfway point of a diagonal line starting off 1,1')
+
   t.end()
 })
-
-describe('midpoint', function(){
-  it('should return the halfway point of a horizontal line starting off 0,0', function(done){
-    var line = t.linestring([[0,0], [10,0]])
-    var pt1 = t.point(0,0)
-    var pt2 = t.point(10, 0)
-    var expectedMidPoint = t.point(5, 0)
-    t.midpoint(pt1, pt2, function(err, midpoint){
-      if(err) throw err
-      _.isEqual(midpoint, expectedMidPoint).should.be.true
-      done()
-    })
-  })
-  it('should return the halfway point of a vertical line starting off 0,0', function(done){
-    var line = t.linestring([[0,0], [0,10]])
-    var pt1 = t.point(0,0)
-    var pt2 = t.point(0,10)
-    var expectedMidPoint = t.point(0, 5)
-    t.midpoint(pt1, pt2, function(err, midpoint){
-      if(err) throw err
-      _.isEqual(midpoint, expectedMidPoint).should.be.true
-      done()
-    })
-  })
-  it('should return the halfway point of a vertical line starting off 1,1', function(done){
-    var line = t.linestring([[1,1], [1,11]])
-    var pt1 = t.point(1,1)
-    var pt2 = t.point(1, 11)
-    var expectedMidPoint = t.point(1, 6)
-    t.midpoint(pt1, pt2, function(err, midpoint){
-      if(err) throw err
-      _.isEqual(midpoint, expectedMidPoint).should.be.true
-      done()
-    })
-  })
-  it('should return the halfway point of a horizontal line starting off 1,1', function(done){
-    var line = t.linestring([[1,1], [11,1]])
-    var pt1 = t.point(1,1)
-    var pt2 = t.point(11,1)
-    var expectedMidPoint = t.point(6, 1)
-    t.midpoint(pt1, pt2, function(err, midpoint){
-      if(err) throw err
-      _.isEqual(midpoint, expectedMidPoint).should.be.true
-      done()
-    })
-  })
-  it('should return the halfway point of a diagonal line starting off 0,0', function(done){
-    var line = t.linestring([[0,0], [10,10]])
-    var pt1 = t.point(0,0)
-    var pt2 = t.point(10,10)
-    var expectedMidPoint = t.point(5, 5)
-    t.midpoint(pt1, pt2, function(err, midpoint){
-      if(err) throw err
-      _.isEqual(midpoint, expectedMidPoint).should.be.true
-      done()
-    })
-  })
-  it('should return the halfway point of a diagonal line starting off 1,1', function(done){
-    var line = t.linestring([[1,1], [11,11]])
-    var pt1 = t.point(1,1)
-    var pt2 = t.point(11,11)
-    var expectedMidPoint = t.point(6, 6)
-    t.midpoint(pt1, pt2, function(err, midpoint){
-      if(err) throw err
-      _.isEqual(midpoint, expectedMidPoint).should.be.true
-      done()
-    })
-  })
-}) 
